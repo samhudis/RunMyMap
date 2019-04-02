c5ccd7f67ee134ae65206a3c6e57ee50ba8003e5
samhudis <samhudisnyc@gmail.com>
2019-04-01 21:19:55 -0400

    session errors now displaying properly

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -4,8 +4,6 @@ import merge from 'lodash/merge';
 
 export default (state = [], action) => {
     switch (action.type) {
-        // case RECEIVE_SESSION_ERRORS:
-        //     return merge ({}, state, {errors: action.errors});
         case RECEIVE_CURRENT_USER:
             const newState =  merge([], state);
             delete newState.errors;
